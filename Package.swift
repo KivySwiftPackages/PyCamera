37dfe0e85a387d8d42505cda16df91db2ae6557a
KivySwiftPackages <riper-chocks0g@icloud.com>
2023-10-12 20:39:40 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
 	name: "PyCamera",
-	platforms: [.iOS(.v14)],
+	platforms: [.iOS(.v13)],
 	products: [
 		.library(name: "PyCamera", targets: ["PyCamera"])
 	],
